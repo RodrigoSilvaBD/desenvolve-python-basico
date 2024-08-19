992bd3a8c51bca2fed35fd420da3a54299ddb90b
RodrigoSilvaBD <rodrigo.silva@pdbomdespacho.com.br>
2024-08-19 19:02:34 -0300

    Delete Modulo5 directory

--- Modulo5/aula2_questao2.py
+++
@@ -1,9 +0,0 @@
-#Escreva uma função em Python chamada soma_quadrados que recebe dois números como parâmetros e retorna a soma dos seus quadrados.
-#No programa principal solicite ao usuário que insira dois números e utilize a função para exibir a soma dos quadrados.
-
-def somaQuadrados(a, b):
-    return a**2 + b**2
-
-num1, num2 = float(input("Digite o primeiro numero: ")), float(input("Digite o segundo numero: "))
-resultado = somaQuadrados(num1, num2)
-print(resultado)
